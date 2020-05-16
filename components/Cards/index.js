12e8e0e35094d7a530cffe30f27cd1cff6d67087
Troy mangrum <tmmangrum@gmail.com>
2020-05-15 22:26:34 -0500

    finally got my images and cards to show up.

--- components/Cards/index.js
+++ components/Cards/index.js
@@ -18,42 +18,61 @@
 // </div>
 //
 // Use your function to create a card for each of the articles and add the card to the DOM.
-function newCardMaker(){
+function newCardMaker(data){
 
 
 	// Elements
     const newCard = document.createElement("div");
     const header = document.createElement("div");
+    const author = document.createElement("div")
     const imageContainer = document.createElement("div");
     const newImg = document.createElement("img");
     const newAuthor = document.createElement("span");
   
   
       //text content
-    header.textContent = "headline";
-    newAuthor.textContent = "authorName";
-    newImg.src = "imgSrc";
+    header.textContent = data.headline;
+    newAuthor.textContent = data.authorName;
+    newImg.src = data.authorPhoto;
       
       //classes
     newCard.classList.add("card");
     header.classList.add("headline");
     imageContainer.classList.add("img-container");
+    author.classList.add('author');
+
   
   
   
   
 
     newCard.appendChild(header);
-    newCard.appendChild(imageContainer);
-    newCard.appendChild(newImg);
-    newCard.appendChild(newAuthor);
-
+    newCard.appendChild(author);
+    author.appendChild(imageContainer);
+    imageContainer.appendChild(newImg);
+    author.appendChild(newAuthor)
 
     return newCard
    
 
 }
+const newEntry = document.querySelector('.cards-container')
 
-.catch(err => {
+
+axios
+  .get("https://lambda-times-backend.herokuapp.com/articles")
+  .then(response => {
+    console.log(response.data.articles);
+    for (let key in response.data.articles) {
+      console.log(key);
+      for (let i = 0; i < response.data.articles[key].length; i++) {
+        // Do whatever you need to
+        const newCard = newCardMaker(response.data.articles[key][i]);
+        console.log(response.data.articles[key][i]);
+        newEntry.appendChild(newCard);
+      }
+    }
+  })
+  .catch(err => {
     console.log(err);
-});
+  });
