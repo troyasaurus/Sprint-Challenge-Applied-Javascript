2ae9cd04ac936f64f376133bbb1aa8e5cd7b9fb0
Troy mangrum <tmmangrum@gmail.com>
2020-05-15 20:18:35 -0500

    finished header, starting tabs"

--- components/Tabs/index.js
+++ components/Tabs/index.js
@@ -7,3 +7,9 @@
 //
 //  Each tab should look like this:
 //    <div class="tab">topic here</div>
+
+
+axios.get('https://lambda-times-backend.herokuapp.com/topics')
+.then((response) => {
+    console.log(response.data);
+})
